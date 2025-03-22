25d28a31c114b1a8dc997c08637bf6fc1a58df61
vhqr0 <zq_cmd@163.com>
2025-03-22 14:02:42 +0800

    update

--- src/clj_lang_extra/core.clj
+++ src/clj_lang_extra/core.clj
@@ -7,6 +7,8 @@
 (defn str->float [^String s] (Double/parseDouble s))
 (defn hex->int [^String s] (Long/parseLong s 16))
 (defn int->hex [^long i] (Long/toHexString i))
+(defn bin->int [^String s] (Long/parseLong s 2))
+(defn int->bin [^long i] (Long/toBinaryString i))
 
 ^:rct/test
 (comment
@@ -14,25 +16,26 @@
   (str->float "123.4") ; => 123.4
   (hex->int "ff") ; => 255
   (int->hex 255) ; => "ff"
+  (bin->int "1011") ; => 11
+  (int->bin 11) ; => "1011"
   )
 
 ;;; inst utils
 
 (defn now [] (Date.))
+(defn ms->inst [^long ms] (Date. ms))
 
 (comment
-  (now))
+  (now)
+  (ms->inst 0))
 
 ;;; exc utils
 
-(defn throwable? [ex] (instance? Throwable ex))
 (defn exception? [ex] (instance? Exception ex))
 (defn ex-info? [ex] (instance? clojure.lang.ExceptionInfo ex))
 
 ^:rct/test
 (comment
-  (throwable? "error") ; => false
-  (throwable? (Exception.)) ; => true
   (exception? (Exception.)) ; => true
   (exception? (ex-info "" {})) ; => true
   (ex-info? (Exception.)) ; => false
@@ -40,3 +43,15 @@
   )
 
 (defn try-catch [f] (try [(f) nil] (catch Exception ex [nil ex])))
+
+^:rct/test
+(comment
+  (try-catch
+   (fn []
+     (throw (ex-info "test error" {}))))
+  ;; =>> [nil ex-info?]
+  (try-catch
+   (fn []
+     (ex-info "test error" {})))
+  ;; =>> [ex-info? nil]
+  )
--- src/clj_lang_extra/core.cljs
+++ src/clj_lang_extra/core.cljs
@@ -6,30 +6,35 @@
 (defn str->float [s] (js/parseFloat s))
 (defn hex->int [s] (js/parseInt s 16))
 (defn int->hex [i] (.toString i 16))
+(defn bin->int [s] (js/parseInt s 2))
+(defn int->bin [i] (.toString i 2))
 
+^:rct/test
 (comment
   (str->int "123") ; => 123
   (str->float "123.4") ; => 123.4
   (hex->int "ff") ; => 255
   (int->hex 255) ; => "ff"
+  (bin->int "1011") ; => 11
+  (int->bin 11) ; => "1011"
   )
 
 ;;; inst utils
 
 (defn now [] (js/Date.))
+(defn ms->inst [ms] (js/Date. ms))
 
 (comment
-  (now))
+  (now)
+  (ms->inst 0))
 
 ;;; exc utils
 
-(defn throwable? [ex] (instance? js/Error ex))
 (defn exception? [ex] (instance? js/Error ex))
 (defn ex-info? [ex] (instance? ExceptionInfo ex))
 
+^:rct/test
 (comment
-  (throwable? "error") ; => false
-  (throwable? (js/Error.)) ; => true
   (exception? (js/Error.)) ; => true
   (exception? (ex-info "" {})) ; => true
   (ex-info? (js/Error.)) ; => false
@@ -37,3 +42,15 @@
   )
 
 (defn try-catch [f] (try [(f) nil] (catch js/Error ex [nil ex])))
+
+^:rct/test
+(comment
+  (try-catch
+   (fn []
+     (throw (ex-info "test error" {}))))
+  ;; =>> [nil ex-info?]
+  (try-catch
+   (fn []
+     (ex-info "test error" {})))
+  ;; =>> [ex-info? nil]
+  )
